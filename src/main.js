a69f2303903dc8226c507b90224d5564c268fe09
zhaizhe520 <zhaizhe520@gmail.com>
2026-05-07 12:52:54 +0800

    restore

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia' // 1. 引入工廠函式
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const pinia = createPinia() // 2. 創建 Pinia 實例
+
+app.use(pinia) // 3. 註冊插件 (這步最關鍵！)
+app.mount('#app')//掛載
