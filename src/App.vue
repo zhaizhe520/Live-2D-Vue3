6ab6012a732e2a036eb38cb0271ab385ad600eae
zhaizhe520 <zhaizhe520@gmail.com>
2026-05-07 10:49:29 +0800

    new

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<!-- App.vue -->
+<script setup>
+import Pet from './components/pet.vue'
+import text1 from './components/text.vue'
+</script>
+
+<template>
+  <text1 />
+  <div class="main-content">
+    <Pet />
+  </div>
+</template>
+
+<style>
+/* 让背景稍微好看点 */
+.main-content {
+  font-family: sans-serif;
+  padding: 50px;
+  background-color: #e8e1e9;
+  min-height: 100vh;
+}
+</style>
---
+++ src/components/text.vue
@@ -0,0 +1,5 @@
+<template>
+
+<h1>你好</h1>
+
+</template>
